b9e8c3e238763218047463ac711b0b4f0a776121
Muhammed Favaz <113675547+favazz-10@users.noreply.github.com>
2024-01-25 19:34:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,26 +51,26 @@ search.addEventListener('click', () => {
 
                  switch (json.weather[0].main) {
                 case "Clear":
-                    image.src="/clear.png"
+                    image.src="clear.png"
                     break;
                 case "Snow":
-                    image.src="/snow.png"
+                    image.src="snow.png"
                     break;
                 case "Clouds":
-                    image.src="/cloud.png"
+                    image.src="cloud.png"
                     break;
                 case "Rain":
-                    image.src="/rain.png"
+                    image.src="rain.png"
                     break;
                 case "Mist":
-                    image.src="/mist.png"
+                    image.src="mist.png"
                     break;
                 case "Haze":
-                    image.src="/mist.png"
+                    image.src="mist.png"
                     break;
             
                 default:
-                    image.src="/cloud.png"
+                    image.src="cloud.png"
             }
 
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
